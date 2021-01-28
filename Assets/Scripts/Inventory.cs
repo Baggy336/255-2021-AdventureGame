ac21bd91d26f676975a5ce0d4020485c111a3907
Erik Howley <howleye@ferris.edu>
2021-01-28 11:12:59 -0500

    1/27 Start of GGJ
    + Testing Scene
    + Scripts folder
    + Singleton Inventory
    + PlayerMovement script

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Use the singleton design pattern
+    private static Inventory _main;
+    public static Inventory main
+    {
+        get { return _main;  }
+        private set { _main = value;  }
+    }
+
+    // List items using booleans here
+    public bool hasPuzzlePiece1 = false;
+    public bool hasPuzzlePiece2 = false;
+    public bool hasPuzzlePiece3 = false;
+    public bool hasSpareCarKey = false;
+    public bool hasDishIngredient = false;
+    public bool hasBaseball = false;
+    public bool hasWardrobeKey = false;
+    public bool hasPianoTuner = false;
+    public bool hasRoomKey = false;
+    public bool hasElectronicFrame = false;
+    public bool hasKeepSake = false;
+
+    private void Start()
+    {
+        if(_main == null)
+        {
+            // Don't destroy the original inventory
+            _main = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else // Destroy any excess inventory systems
+        {
+            Destroy(gameObject);
+        }      
+    }
+}
+   
